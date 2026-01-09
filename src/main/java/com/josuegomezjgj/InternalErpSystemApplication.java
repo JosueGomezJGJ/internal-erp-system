4ab0f3f9f5ff43eda01fd027564527dabe5b3d7b
Josue G <josue.gomez@mavs.uta.edu>
2026-01-08 20:59:15 -0600

    chore: inital spring boot project setup

---
+++ src/main/java/com/josuegomezjgj/InternalErpSystemApplication.java
@@ -0,0 +1,13 @@
+package com.josuegomezjgj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalErpSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalErpSystemApplication.class, args);
+    }
+
+}
